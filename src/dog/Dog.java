773543fe45accf4684f20be2d6c72f5eaef4e6cb
mofumofuchan <you_oki1@yahoo.co.jp>
2016-09-19 14:50:11 +0900

    Dogクラスを実装

--- src/dog/Dog.java
+++ src/dog/Dog.java
@@ -1,5 +1,18 @@
 package dog;
 
 public class Dog {
-
+	private String nakigoe = "Ç…Ç·Å[ÇÒ";
+	private String name;
+	
+	public Dog() {
+		name = "å¢";
+	}
+	
+	public Dog(string name) {
+		this.name = name;
+	}
+	
+	public void naku() {
+		System.out.println(name + ": " + nakigoe);
+	}
 }
