d440eb6da243b35a275350b5b10fd33fa933f7a2
kt <lim.intefx@gmail.com>
2016-09-19 15:25:28 +0900

    完成
    バグ修正

--- src/dog/Dog.java
+++ src/dog/Dog.java
@@ -3,15 +3,15 @@
 public class Dog {
 	private String nakigoe = "ワン、ワンワン";
 	private String name;
-	
+
 	public Dog() {
 		name = "犬山";
 	}
-	
-	public Dog(string name) {
+
+	public Dog(String name) {
 		this.name = name;
 	}
-	
+
 	public void naku() {
 		System.out.println(name + ": " + nakigoe);
 	}
